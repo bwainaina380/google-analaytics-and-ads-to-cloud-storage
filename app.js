1b4448b796eb8cd386ecd174dd481d7812714251
bwainaina380 <bwainaina380@gmail.com>
2020-01-12 15:46:03 +0300

    The app is pulling data from GA and pushing it to Google Cloud Storage

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const views = require("./lib/views.json");
+const googleAnalytics = require("./lib/googleAnalytics");
+const googleAds = require("./lib/googleAds");
+const dotenv = require("dotenv");
+const storage = require("./lib/cloudStorage");
 
+dotenv.config();
 app.set("port", process.env.PORT || 3000);
+
+views.forEach(view => {
+  googleAnalytics(view.name, view.id);
+});
+
+views.forEach(view => {
+  googleAds(view.name, view.id);
+});
 
 app.listen(app.get("port"), () => {
   console.log(
