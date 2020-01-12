82b1d052421ecfc3166e8bf1bfcdce2cfff73dcd
bwainaina380 <bwainaina380@gmail.com>
2020-01-12 22:31:16 +0300

    Using settimeout to limit the number of requests at any given time and ensuring the functions run without an error

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const views = require("./lib/views.json");
 const googleAnalytics = require("./lib/googleAnalytics");
 const googleAds = require("./lib/googleAds");
 const dotenv = require("dotenv");
+const uploadFile = require("./lib/cloudStorage");
 
 dotenv.config();
 app.set("port", process.env.PORT || 3000);
@@ -12,9 +13,22 @@ views.forEach(view => {
   googleAnalytics(view.name, view.id);
 });
 
-views.forEach(view => {
-  googleAds(view.name, view.id);
-});
+setTimeout(() => {
+  views.forEach(view => {
+    googleAds(view.name, view.id);
+  });
+}, 30000);
+
+const files = [
+  __dirname + "/lib/results/analytics.json",
+  __dirname + "/lib/results/ads.json"
+];
+
+setTimeout(() => {
+  files.forEach(file => {
+    uploadFile(file);
+  });
+}, 60000);
 
 app.listen(app.get("port"), () => {
   console.log(
