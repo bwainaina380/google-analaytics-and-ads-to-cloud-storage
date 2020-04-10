196a84f45571e19745e4652e8cd68951a2e24878
Brian Wainaina <bwainaina380@gmail.com>
2020-04-10 16:40:59 +0300

    Added cron to run the app once every day at 4 AM

--- app.js
+++ app.js
@@ -6,52 +6,61 @@ const googleAds = require("./lib/googleAds");
 const dotenv = require("dotenv");
 const uploadFile = require("./lib/cloudStorage");
 const fs = require("fs");
+const cron = require("node-cron");
 
 dotenv.config();
 app.set("port", process.env.PORT || 3000);
 
-try {
-  views.forEach((view) => {
-    googleAnalytics(view.name, view.id);
-  });
-} catch (error) {
-  console.error(error);
-}
-
-setTimeout(() => {
+cron.schedule("0 4 * * *", () => {
   try {
     views.forEach((view) => {
-      googleAds(view.name, view.id);
+      googleAnalytics(view.name, view.id);
     });
   } catch (error) {
     console.error(error);
   }
-}, 30000);
+});
+
+cron.schedule("0 4 * * *", () => {
+  setTimeout(() => {
+    try {
+      views.forEach((view) => {
+        googleAds(view.name, view.id);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, 30000);
+});
 
 const files = [
   __dirname + "/lib/results/analytics.json",
   __dirname + "/lib/results/ads.json",
 ];
 
-setTimeout(() => {
-  try {
-    files.forEach((file) => {
-      uploadFile(file);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}, 60000);
+cron.schedule("0 4 * * *", () => {
+  setTimeout(() => {
+    try {
+      files.forEach((file) => {
+        uploadFile(file);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, 60000);
+});
 
-setTimeout(() => {
-  try {
-    files.forEach((file) => {
-      fs.unlinkSync(file);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}, 120000);
+cron.schedule("0 4 * * *", () => {
+  setTimeout(() => {
+    try {
+      files.forEach((file) => {
+        fs.unlinkSync(file);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, 120000);
+});
 
 app.listen(app.get("port"), () => {
   console.log(
